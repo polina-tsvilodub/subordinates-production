a8d276fe7d255ef6a665a8b12ded1562267b832d
polina-tsvilodub <42241414+polina-tsvilodub@users.noreply.github.com>
2019-08-08 18:07:52 +0200

    initialized replication of first exp with 10 trials

---
+++ exp1-replica/05_views.js
@@ -0,0 +1,191 @@
+// In this file you can instantiate your views
+// We here first instantiate wrapping views, then the trial views
+
+
+/** Wrapping views below
+
+* Obligatory properties
+
+    * trials: int - the number of trials this view will appear
+    * name: string
+
+*Optional properties
+    * buttonText: string - the text on the button (default: 'next')
+    * text: string - the text to be displayed in this view
+    * title: string - the title of this view
+
+    * More about the properties and functions of the wrapping views - https://babe-project.github.io/babe-docs/01_designing_experiments/01_template_views/#wrapping-views
+
+*/
+
+// Every experiment should start with an intro view. Here you can welcome your participants and tell them what the experiment is about
+
+var speaker = _.sample(["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles"]);
+var listener = _.sample(["Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Margaret"]);
+
+
+const botcaptcha = custom_botcaptcha({
+  name: 'botcaptcha',
+  trials: 1,
+  story: speaker + ' says to ' + listener + ': "It\'s a beautiful day, isn\'t it?"',
+  question: "Who is " + speaker + " talking to?",
+  speaker: speaker,
+  listener: listener
+
+});
+
+
+// For most tasks, you need instructions views
+const instructions = babeViews.view_generator("instructions",{
+    trials: 1,
+    name: 'instrucions',
+    title: 'Instructions',
+    text:  `Please read the instructions carefully before starting the experiment and
+            make sure you can concentrate on the task without disturbance.
+
+            <br />
+            <br />
+            The experiment consists of three rounds, each having a few warm-up trials before the main trials start.
+            <br />
+            <br />
+            During the warm-up trials, you will see pictures of objects.
+            <br />
+            <br />
+            Please label the objects. You will be given feedback on your labels.
+            <br />
+            <br />
+            By pressing the button 'Go to Trials' you begin the first round.
+
+            <br/>
+            <br/>
+
+
+            `,
+    buttonText: 'go to trials'
+});
+
+
+// In the post test questionnaire you can ask your participants addtional questions
+const custom_post_test = custom_post_test_view({
+  name: 'post_test',
+  title: 'Additional information',
+  text: `Please enter your native languages.
+  <br />
+    Answering the other questions is optional, but your answers will help us analyze our results.`,
+  trials: 1
+});
+const custom_intro = custom_intro_view({
+  name: 'Intro',
+  title: 'Welcome!',
+  picture1: 'images/cpl.png',
+  trials: 1
+
+});
+// The 'thanks' view is crucial; never delete it; it submits the results!
+const thanks = babeViews.view_generator("thanks", {
+    trials: 1,
+    name: 'thanks',
+    title: 'Thank you for taking part in this experiment!',
+    prolificConfirmText: 'Press the button'
+});
+
+
+const warmup_trials1 = custom_textfield_warmup({
+  name: 'warmup1',
+  title: 'Labeling task',
+  trials: 4,
+  data: trial_info.text_insertion_warmup1,
+//  hook: {
+//    after_response_enabled: check_response
+//  }
+});
+const warmup_trials2 = custom_textfield_warmup({
+  name: 'warmup2',
+  title: 'Labeling task',
+  trials: 3,
+  data: trial_info.text_insertion_warmup2,
+//  hook: {
+//    after_response_enabled: check_response
+//  }
+});
+
+const warmup_trials3 = custom_textfield_warmup({
+  name: 'warmup3',
+  title: 'Labeling task',
+  trials: 3,
+  data: trial_info.text_insertion_warmup3,
+//  hook: {
+//    after_response_enabled: check_response
+//  }
+});
+
+const context1 = babeViews.view_generator("instructions",{
+    trials: 1,
+    name: 'instructions',
+    title: 'Instructions',
+    text:  `
+    Next, you will complete the main trials.
+    <br/>
+    <br/>
+    You are watching groups of objects and describing them as they pass by.
+    <br />
+    <br />
+
+    Press the button 'Go to trials' to begin the main trials.
+            `,
+    buttonText: 'go to trials'
+});
+const context2 = babeViews.view_generator("instructions",{
+    trials: 1,
+    name: 'instructions',
+    title: 'Instructions',
+    text:  `
+    Now the second round of the experiment starts. You will complete similar trials.
+    <br />
+    </br>
+    Press the button 'Go to trials' to begin the second round.
+            `,
+    buttonText: 'go to trials'
+});
+
+const context3 = babeViews.view_generator("instructions",{
+    trials: 1,
+    name: 'instructions',
+    title: 'Instructions',
+    text:  `
+    Now the last round of the experiment starts. You will complete similar trials.
+    <br />
+    </br>
+    Press the button 'Go to trials' to begin the last round.
+            `,
+    buttonText: 'go to trials'
+});
+
+const main_trials1 = custom_textfield_main({
+  name: 'main1',
+//  title: 'Parades',
+  trials: 4,
+  data: _.shuffle(trial_info.text_insertion_main1)
+
+
+});
+const main_trials2 = custom_textfield_main({
+  name: 'main2',
+//  title: 'Parades',
+  trials: 3,
+  data: _.shuffle(trial_info.text_insertion_main2)
+
+
+});
+const main_trials3 = custom_textfield_main({
+  name: 'main3',
+//  title: 'Parades',
+  trials: 3,
+  data: _.shuffle(trial_info.text_insertion_main3)
+
+
+});
+
+// There are many more templates available:
+// forced_choice, slider_rating, dropdown_choice, testbox_input, rating_scale, image_selection, sentence_choice,
+// key_press, self_paced_reading and self_paced_reading_rating_scale
---
+++ exp1-replica/06_main.js
@@ -0,0 +1,63 @@
+// In this file you initialize and configure your experiment using babeInit
+
+$("document").ready(function() {
+    // prevent scrolling when space is pressed
+    window.onkeydown = function(e) {
+        if (e.keyCode === 32 && e.target === document.body) {
+            e.preventDefault();
+        }
+    };
+
+    // calls babeInit
+    // in debug mode this returns the babe-object, which you can access in the console of your browser
+    // e.g. >> window.babe_monitor or window.babe_monitor.findNextView()
+    // in all other modes null will be returned
+    window.babe_monitor = babeInit({
+        // You have to specify all views you want to use in this experiment and the order of them
+        views_seq: [
+
+            custom_intro,
+            botcaptcha,
+            instructions,
+            warmup_trials1,
+            context1,
+            main_trials1,
+            context2,
+            warmup_trials2,
+            main_trials2,
+            context3,
+            warmup_trials3,
+            main_trials3,
+            custom_post_test,
+            thanks,
+        ],
+        // Here, you can specify all information for the deployment
+        deploy: {
+            experimentID: "4",
+            serverAppURL: "https://pragmatics-ptb.herokuapp.com/api/submit_experiment/",
+            // Possible deployment methods are:
+            // "debug" and "directLink"
+            // As well as "MTurk", "MTurkSandbox" and "Prolific"
+            deployMethod: "debug",
+            contact_email: "polinats@mit.edu",
+            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+        },
+        unqiueTurkerID: '0e8b1d8dfda437c9dfeff65c57c17e40',
+
+        // Here, you can specify how the progress bar should look like
+        progress_bar: {
+            in: [
+                // list the view-names of the views for which you want a progress bar
+                warmup_trials1.name,
+                main_trials1.name,
+                warmup_trials2.name,
+                main_trials2.name,
+                warmup_trials3.name,
+                main_trials3.name
+            ],
+             // Possible styles are "default", "separate" and "chunks"
+            style: "separate",
+            width: 100
+        }
+    });
+});
