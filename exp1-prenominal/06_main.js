312cc08e363cd1f99e3bcc8903abbcba748f8700
polina-tsvilodub <42241414+polina-tsvilodub@users.noreply.github.com>
2019-08-21 20:07:00 +0200

    set data retrieval

--- exp1-prenominal/06_main.js
+++ exp1-prenominal/06_main.js
@@ -33,7 +33,7 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "8",
+            experimentID: "10",
             serverAppURL: "https://pragmatics-ptb.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
